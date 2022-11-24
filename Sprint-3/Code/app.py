d0d6046fcf7eb744f260443fee78f0c6d2d01ef3
pooj2002 <poojasukumar1409@gmail.com>
2022-11-24 20:22:12 +0530

    added bcrypt

--- Sprint-3/Code/app.py
+++ Sprint-3/Code/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session , make_response, url_for, json, flash
 import re, ibm_db
+from flask_bcrypt import Bcrypt 
 import ibm_db_dbi ,pandas as pd
 from flask_mail import Mail, Message
 import os, datetime
@@ -12,8 +13,10 @@ app = Flask(__name__)
 
 app.secret_key = 'SECRET_KEY'
 
+#added for bcrypt
+bcrypt = Bcrypt(app)
 
-
+#ibm database connection
 conn = ibm_db.connect("DATABASE=bludb; HOSTNAME=b70af05b-76e4-4bca-a1f5-23dbb4c6a74e.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud; PORT=32716; SECURITY=SSL; SSLServerCertificate=DigiCertGlobalRootCA.crt; UID=fks81181;PWD=mdQZREsASRiq3Lb1",'','')
 pd_conn = ibm_db_dbi.Connection(conn)
 
@@ -40,6 +43,8 @@ def register():
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
+        #added for bcrypt
+        password = bcrypt.generate_password_hash(password)
         
         sql = 'SELECT * from REGISTER WHERE USERNAME = ?'
         stmt = ibm_db.prepare(conn, sql)
@@ -83,16 +88,17 @@ def login():
         username = request.form['username']
         password = request.form['password'] 
         
-        sql = 'SELECT * from REGISTER WHERE USERNAME = ? AND PASSWORD = ?'
+        sql = 'SELECT * from REGISTER WHERE USERNAME = ?'
         stmt = ibm_db.prepare(conn, sql)
         ibm_db.bind_param(stmt, 1, username)
-        ibm_db.bind_param(stmt, 2, password)
+        
         ibm_db.execute(stmt)
-
+       
         account = ibm_db.fetch_assoc(stmt)
         
         print (account)
-        if account:
+        #changed to add bcrypt
+        if account and bcrypt.check_password_hash(account['PASSWORD'], password) :
             session['loggedin'] = True
             session['id'] = account['USER_ID']
             userid =  account['USER_ID']
